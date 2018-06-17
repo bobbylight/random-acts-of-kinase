00a01ce9f74bb16fb8170532b556e4f6ad98c062
bobby_light <robert.e.futrell@gmail.com>
2018-06-17 17:51:40 -0400

    Fixing broken unit test

--- src/test/java/org/sgc/rak/rest/CsvHttpMessageConverterTest.java
+++ src/test/java/org/sgc/rak/rest/CsvHttpMessageConverterTest.java
@@ -71,9 +71,10 @@ public class CsvHttpMessageConverterTest {
         ParameterizedType type = TypeUtils.parameterize(PagedDataRep.class, Compound.class);
         converter.writeInternal(pagedDataRep, type, outputMessage);
 
-        String expected = "compoundName,chemotype,s10,smiles,source,primaryReference,primaryReferenceUrl,hidden\n" +
-            "compoundA,,0.3,smilesA,,,,false\n" +
-            "compoundB,,,smilesB,,,,false\n";
+        String expected =
+            "compoundName,chemotype,s10,smiles,source,primaryReference,primaryReferenceUrl,hidden,solubility\n" +
+            "compoundA,,0.3,smilesA,,,,false,\n" +
+            "compoundB,,,smilesB,,,,false,\n";
         Assert.assertEquals(expected, outputMessage.getBodyAsString());
     }
 }
